be4fa747a0223b3747bd92c423ccf1b0a21ff9a5
Aditya <adityapradhan@tudotechnologies.com>
2026-08-15 13:38:18 +0530

    feat: update RootLayout to display game closure message with centered styling

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+        
+        🎉🎉🎉🎉Game is Closed!! 
+          Bye Bye!!
+         🎉🎉🎉🎉</body>
     </html>
   );
 }
